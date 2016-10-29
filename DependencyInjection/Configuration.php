c83d8c8418136af3c4934e795df509ac8f34cdcf
Jos635 <Jos635@outlook.com>
2016-10-29 14:49:27 +0200

    Added timeout configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -22,6 +22,7 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
             ->scalarNode('api_token')->defaultNull()->end()
+            ->scalarNode('timeout')->defaultValue(10)->end()
             ->end()
         ;
         return $treeBuilder;
